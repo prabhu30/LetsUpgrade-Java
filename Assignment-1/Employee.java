2362bc8865f763be2775c4ff419c52a4de8bc7c0
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2020-10-10 15:48:28 +0530

    Update Employee.java

--- Assignment-1/Employee.java
+++ Assignment-1/Employee.java
@@ -5,9 +5,9 @@ public class Employee{
 	int age = 25;
 	
 	public void display(){
-			System.out.println("The name is "+name);
-			System.out.println("The age is "+age);
-			System.out.println("The city is "+city);
+		System.out.println("The name is "+name);
+		System.out.println("The age is "+age);
+		System.out.println("The city is "+city);
 	}
 	
 	public static void main(String[] args){
